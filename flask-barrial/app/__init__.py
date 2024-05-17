b13b2557514690ac296b42eb732d9d69e800f7aa
Alejandro Biarrieta <103573136+Reinapepeada@users.noreply.github.com>
2024-05-17 18:08:52 -0300

    agregar script para DB

--- flask-barrial/app/__init__.py
+++ flask-barrial/app/__init__.py
@@ -2,14 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
 
     db.init_app(app)
-    migrate.init_app(app, db)
 
     from .routes import item_bp
     app.register_blueprint(item_bp, url_prefix='/api/items')
